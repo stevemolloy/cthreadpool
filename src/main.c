fb068f117ea24ac8b6ba0f006f1e281b2d10ad51
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-10-24 01:51:22 +0200

    Basic structure set

--- src/main.c
+++ src/main.c
@@ -3,37 +3,44 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define NUMJOBS 10
+#define NUMJOBS 100
+#define NUMTHREADS 5
+#define SLEEP_US 500000
 
 int slow_square(int val) {
-  usleep(500000);
-
-  return val*val;
+    usleep(SLEEP_US);
+    return val * val;
 }
 
 void *wrapper(void *v_ptr) {
-  int val = *(int *)v_ptr;
+    int *val_ptr = (int *)v_ptr;
+    int val = *val_ptr;
 
-  int *res = malloc(sizeof(int));
-
-  *res = slow_square(val);
-
-  return (void*)res;
+    int *res = malloc(sizeof(int));
+    *res = slow_square(val);
+    return (void *)res;
 }
 
 int main(void) {
-  // pthread_t threads[NUMJOBS];
+    pthread_t threads[NUMJOBS];
 
-  for (int i=0; i<NUMJOBS; i++) {
-    int input = i * 1;
-    void *v_ptr = (void*)&input;
+    int inputs[NUMJOBS];
+    for (int i = 0; i < NUMJOBS; i++) {
+        inputs[i] = i * 1;
+        pthread_create(&threads[i], NULL, &wrapper, &inputs[i]);
+    }
 
-    void *res_ptr = wrapper(v_ptr);
+    void *res_ptr[NUMJOBS];
 
-    int *res = (int*)res_ptr;
+    for (int i = 0; i < NUMJOBS; i++) {
+        pthread_join(threads[i], &res_ptr[i]);
 
-    printf("%d squared is %d\n", input, *res);
-  }
+        int *res = (int *)res_ptr[i];
 
-  return 0;
+        printf("%d squared is %d\n", i * 1, *res);
+        free(res); // Free the allocated memory
+    }
+
+    return 0;
 }
+
