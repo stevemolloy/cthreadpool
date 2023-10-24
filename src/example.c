03cf8cd9a1d8001a193483a4c0d9813c1b821dc9
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-10-24 15:33:50 +0200

    Comments and tidy-up

--- src/example.c
+++ src/example.c
@@ -13,33 +13,39 @@ typedef struct {
 } WrapperInput;
 
 int slow_square(int val) {
-    usleep(SLEEP_US);
-    return val * val;
+  usleep(SLEEP_US);
+  return val * val;
 }
 
 void *wrapper(void *v_ptr) {
-    WrapperInput *wi = (WrapperInput *)v_ptr;
+  WrapperInput *wi = (WrapperInput *)v_ptr;
 
-    wi->out_val = slow_square(wi->in_val);
+  wi->out_val = slow_square(wi->in_val);
 
-    return NULL;
+  return NULL;
 }
 
 int main(void) {
-    pthread_t threads[NUMJOBS];
-    WrapperInput inputs[NUMJOBS];
+  // For this to work we need an array of threads and an array of structs used as inputs
+  pthread_t threads[NUMJOBS];
+  WrapperInput inputs[NUMJOBS];
 
-    for (int i = 0; i < NUMJOBS; i++) {
-      inputs[i].in_val = i;
-      pthread_create(&threads[i], NULL, &wrapper, &inputs[i]);
-    }
+  // Prepare the input and create the threads
+  for (int i = 0; i < NUMJOBS; i++) {
+    inputs[i].in_val = i;
+    pthread_create(&threads[i], NULL, &wrapper, &inputs[i]);
+  }
 
-    for (int i = 0; i < NUMJOBS; i++) {
-        pthread_join(threads[i], NULL);
+  // Wait for the threads to complete
+  for (int i = 0; i < NUMJOBS; i++) {
+      pthread_join(threads[i], NULL);
+  }
 
-        printf("%d squared is %d\n", i * 1, inputs[i].out_val);
-    }
+  // Print results
+  for (int i = 0; i < NUMJOBS; i++) {
+      printf("%d squared is %d\n", i * 1, inputs[i].out_val);
+  }
 
-    return 0;
+  return 0;
 }
 
